cc96fa9b5bd4a1c916a6e54f871c54ca086b128c
Denys Klymenko <suncoin@gmail.com>
2019-12-15 21:26:31 +0200

    Initial commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+
+        int count = 0;
+        int[] hourglassSums = new int[16];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                hourglassSums[count] = arr[j][i] + arr[j][i+1] + arr[j][i+2] +  arr[j+1][i+1] + arr[j+2][i] + arr[j+2][i+1] + arr[j+2][i+2];
+                count++;
+            }
+        }
+
+        Arrays.sort(hourglassSums);
+        return hourglassSums[15];
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
